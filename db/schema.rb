d14d06ee388c5aa46831583039c00e381ce94880
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2020-10-22 12:59:49 +0630

    Added Category Reference to Entry

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_050622) do
+ActiveRecord::Schema.define(version: 2020_10_22_061714) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 2020_10_22_050622) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "category_id"
+    t.index ["category_id"], name: "index_entries_on_category_id"
     t.index ["user_id"], name: "index_entries_on_user_id"
   end
 
@@ -49,5 +51,6 @@ ActiveRecord::Schema.define(version: 2020_10_22_050622) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "entries", "categories"
   add_foreign_key "entries", "users"
 end
