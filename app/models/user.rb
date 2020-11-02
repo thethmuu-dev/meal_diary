7d546ccd04132af8a54687c6ccafb9eaecba1b0c
Thet Hmuu Eain Soe <thethmuueinsoe@gmail.com>
2020-11-02 17:40:52 +0630

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  before_save { self.email = email.downcase }
+  validates :username, presence: true,
+                      uniqueness: { case_sensitive: false },
+                      length: { minimum: 3, maximum: 25 }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    uniqueness: { case_sensitive: false },
+                    length: { maximum: 105 },
+                    format: { with: VALID_EMAIL_REGEX }
+
+  validates :password, presence: true
+
   def following?(other_user)
     active_relationships.find_by(followed_id: other_user.id)
   end
